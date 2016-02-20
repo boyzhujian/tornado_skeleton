d0521ca5b47cd63dbda6b9bfcf203c525aa30cbd
boyzhujian <610576334@qq.com>
2016-02-20 17:37:34 +0800

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -18,3 +18,12 @@ class logoutHandler(tornado.web.RequestHandler):
 
     def  post(self):
         pass
+    
+    
+class signupHandler(tornado.web.RequestHandler):
+    def  get(self):
+       pass
+
+
+    def  post(self):
+        pass
